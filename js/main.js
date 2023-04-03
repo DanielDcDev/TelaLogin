7a57ad8bf2046b0a7359cc95faa6744b0bdfd90f
Dc Dev <96603256+DanielDcDev@users.noreply.github.com>
2023-04-03 11:12:30 -0300

    atualização

--- js/main.js
+++ js/main.js
@@ -1,13 +1,23 @@
-const name = document.getElementById('.name');
-const email = document.getElementById('.email');
-const  nameCount= document.getElementById('.nameCount');
-const  born= document.getElementById('.born');
-const  password= document.getElementById('.password');
-const  cPassword= document.getElementById('.confirmPassword');
-const  btnCadaster= document.getElementById('.button_cadaster');
-
-
-btnCadaster.addEventListener('click', showRespost);
-const showRespost = () =>{
-    console.log(name);
+'use strict'
+const fillFields = (client) =>{
+    document.getElementById('name')
+    document.getElementById('name')
+    document.getElementById('name')
+    document.getElementById('name')
+    document.getElementById('name')
+    document.getElementById('name')
+    document.getElementById('name')
 }
+const dados = document.getElementById('dados');
+const btnCadaster = document.getElementById('confirm');
+
+const nextButton = () =>{
+    console.log('teste');
+    console.log(nameCli);
+}
+
+btnCadaster.addEventListener('submit', nextButton);
+dados.addEventListener()
+
+
+
